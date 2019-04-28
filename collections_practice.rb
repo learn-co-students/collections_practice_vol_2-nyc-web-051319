59b9458982d3d41a171a5795ee0faeaea9e7670a
Elma Rahman <elmarahman635@gmail.com>
2019-04-28 23:57:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,5 @@
 # your code goes here
+require 'pry'
 
 def begins_with_r(tools_in_array)
       tools_in_array.all? do |tool|
@@ -35,37 +36,51 @@ def remove_non_strings(array)
 end
 
 
-#def count_elements(array)
-
-  #array.inject(...) do |category, (key, value)|
-  #  category[value] += 1
-#  end
-#end
-#end
-
-def merge_data
-
+#prev called the inject method but didn't work
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
 end
 
-def find_cool(hash)
-  hash.select do |key|
-    if key.has_value?("cool")
-      return hash
+#Need explanation on this
+def merge_data(keys,values)
+  keys.each do |name_hash|
+    values.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
       end
     end
-  hash
+  end
+
+
+
+def find_cool(array)
+  array.select do |hash| #select would only return elements in collection for whatever's true from the block
+    #select returns all that is true from block of code
+      hash[:temperature] == "cool" #.has_value? ("cool") #we can directly call the key
+    #  array
+    #  end
+    #end
+ end
 end
 
 def organize_schools(schools)
+
     by_location = {}
-      schools.each do |school, location|
-        location.each do |location_k, location_v|
-          if by_location[location_v] == nil
-            by_location[location_v] = [school]
+      schools.each do |school, location_hash|
+        location = location_hash[:location] #finds location as string
+          #by_location[location_hash[:location]] = []
+          # by_location[location] << school
+          #binding.pry
+          if by_location[location]
+            by_location[location] << school
           else
-            by_location[location_v] << school
+            by_location[location] =[]
+             by_location[location] << school
           end
-        end
         by_location
       end
     end

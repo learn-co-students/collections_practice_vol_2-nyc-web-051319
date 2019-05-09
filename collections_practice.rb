7f0645f76d3870777f92b750c1b6d0103adfb2a2
David Greenspan <ramigreenspan@gmail.com>
2019-05-09 05:43:59 -0400

    collec tions practice

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,35 +1,66 @@
 require 'pry'
 
-def begins_with_r(arg)
-arg.all? do |x|
+def begins_with_r(array)
+array.all? do |x|
 x[0] == "r"
 end
 end
 
-def contain_a(arg)
-  arg.select do |x|
+def contain_a(array)
+  array.select do |x|
   x.include?("a")
 end
 end
 
-def first_wa(arg)
-  arg.detect do |x|
+def first_wa(array)
+  array.detect do |x|
     x[0,2] == "wa"
   end
 end
 
-def remove_non_strings(arg)
-  arg.delete_if do |x|
+def remove_non_strings(array)
+  array.delete_if do |x|
     x.is_a?(String) == false
   end
 end
 
-def count_elements(arg)
+def count_elements(array)
   new_array =[]
-binding.pry
-  arg.each  do |x|
-x.push(counter)
-new_array << x
-  end
-new_array
+  a = 0
+  b = 0
+
+  array.each  do |hash_name|
+hash_name[:count] = 0
+  #
+  name = hash_name[:name]
+# if hash_name[:name] == "blake"
+#  b +=1
+# elsif hash_name[:name] == "ashley"
+#  a +=1
+array.each do |hash|
+
+   if name == hash[:name]
+     hash_name[:count] +=1
+
+end
+end
+end.uniq
+
+end
+
+def merge_data(keys, data)
+  merged = []
+  keys.each {|x| data.first.map {|k,v| if x.values[0] == k then merged << x.merge(v) end}}
+  merged
+end
+
+
+def find_cool(cool)
+    cool.select {|x| x.any? {|k,v| v == "cool"}}
+end
+
+def organize_schools(schools)
+    locations_hash = {}
+    schools.collect {|k,v| locations_hash[v[:location]] = []}
+    locations_hash.each {|k,v| schools.each {|kk,vv| if k == vv[:location] then v << kk  end}}
 end
